3cda50e718e93321100ce5f637735f79c5669619
SumLare <anohin56n@gmail.com>
2018-01-14 23:35:25 +0300

    Add redigo

--- main.go
+++ main.go
@@ -1,9 +1,12 @@
 package main
 
 import (
+	"crypto/rand"
 	"encoding/json"
 	"fmt"
 	"github.com/Syfaro/telegram-bot-api"
+	"github.com/garyburd/redigo/redis"
+	"io"
 	"log"
 	"net/http"
 	"os"
@@ -24,12 +27,12 @@ type Message struct {
 }
 
 func main() {
-	http.HandleFunc("/hooks/circle", handleCircleHook)
+	http.HandleFunc("/hooks/circle", payloadHandler)
 
 	port := ":" + os.Getenv("PORT")
 	log.Printf("Listening on %s...\n", port)
 	if err := http.ListenAndServe(port, nil); err != nil {
-		panic(err)
+		log.Println(err)
 	}
 
 	handleMessages()
@@ -38,8 +41,14 @@ func main() {
 func handleMessages() {
 	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
-		log.Panic(err)
+		log.Println(err)
 	}
+
+	c, err := redis.DialURL(os.Getenv("REDIS_URL"))
+	if err != nil {
+		log.Println(err)
+	}
+	defer c.Close()
 
 	u := tgbotapi.NewUpdate(0)
 	u.Timeout = 60
@@ -54,21 +63,40 @@ func handleMessages() {
 
 		log.Printf("[%s] %s", update.Message.From.UserName, update.Message.Text)
 
+		chatID := update.Message.Chat.ID
 		switch update.Message.Command() {
 		case "start":
 			reply = "CircleCI Bot helps track your build status"
+		case "add":
+			key, _ := newUUID()
+			c.Do("APPEND", chatID, key)
+			reply = "Your circleci key is " + key
 		}
 
-		msg := tgbotapi.NewMessage(update.Message.Chat.ID, reply)
+		msg := tgbotapi.NewMessage(chatID, reply)
 		bot.Send(msg)
 	}
 }
 
-func handleCircleHook(rw http.ResponseWriter, req *http.Request) {
-	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
+func payloadHandler(rw http.ResponseWriter, req *http.Request) {
+	if r.Method != "POST" {
+		w.WriteHeader(http.StatusMethodNotAllowed)
+		return
+	}
 
 	var m Message
-	err = json.NewDecoder(req.Body).Decode(&m)
+	err := json.NewDecoder(req.Body).Decode(&m)
+	if err != nil {
+		w.Header().Set("Content-Type", "application/json; charset=UTF-8")
+		w.WriteHeader(http.StatusBadRequest)
+		return
+	}
+
+	sendMessage(m)
+}
+
+func sendMessage(m Message) {
+	bot, err := tgbotapi.NewBotAPI(os.Getenv("BOT_TOKEN"))
 	if err != nil {
 		log.Println(err)
 	}
@@ -84,4 +112,15 @@ func handleCircleHook(rw http.ResponseWriter, req *http.Request) {
 		p.Commit, p.VCSRevision[:7], p.BuildingTime/1000)
 	chatID, err := strconv.ParseInt(os.Getenv("CHAT_ID"), 10, 64)
 	bot.Send(tgbotapi.NewMessage(chatID, text))
+}
+
+func newUUID() (string, error) {
+	uuid := make([]byte, 16)
+	n, err := io.ReadFull(rand.Reader, uuid)
+	if n != len(uuid) || err != nil {
+		return "", err
+	}
+	uuid[8] = uuid[8]&^0xc0 | 0x80
+	uuid[6] = uuid[6]&^0xf0 | 0x40
+	return fmt.Sprintf("%x-%x-%x-%x-%x", uuid[0:4], uuid[4:6], uuid[6:8], uuid[8:10], uuid[10:]), nil
 }
